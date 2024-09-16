cfa0513ecb3e06c75a96297a61e094666aa7dacd
BrunoMatheus10 <brunomatheus88300432@gmail.com>
2024-09-16 18:04:34 -0300

    Add files via upload

---
+++ atividade7.py
@@ -0,0 +1,21 @@
+# Programa para calcular a quantidade de notas de 50, 10 e 1 necessárias para pagar uma conta
+
+# Solicita ao usuário que insira o valor da conta
+valor = int(input("Digite o valor da conta: "))
+
+# Calcula a quantidade de notas de 50
+notas_50 = valor // 50
+resto = valor % 50
+
+# Calcula a quantidade de notas de 10
+notas_10 = resto // 10
+resto = resto % 10
+
+# Calcula a quantidade de notas de 1
+notas_1 = resto
+
+# Exibe o resultado
+print(f"Para pagar a conta de R${valor}, você precisará de:")
+print(f"{notas_50} nota(s) de 50")
+print(f"{notas_10} nota(s) de 10")
+print(f"{notas_1} nota(s) de 1")
